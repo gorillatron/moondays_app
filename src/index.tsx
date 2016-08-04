1e54e5ecf4884c2b5e2648a714dc4eb8f0a7e799
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-08-04 17:56:13 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+
+class C extends React.Component<{}, {}> {
+
+  render() {
+    return <div>lol testr</div>
+  }
+
+}
+
+ReactDOM.render(
+    <C/>,
+    document.body
+);
