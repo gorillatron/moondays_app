eba1b1b5663206314ee26c6a2b5a7be38a027fa8
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2016-08-05 22:33:57 +0200

    werk

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -5,6 +5,11 @@ import * as moondays from 'moondays'
 import * as suncalc from 'suncalc'
 import * as moment from 'moment'
 
+const colors = {
+  moon: "#d5d9d9",
+  moonShadow: "#969999"
+}
+
 
 
 interface MoonProps {
@@ -21,10 +26,10 @@ const Moon = (properties:MoonProps) => {
     <div style={{
       position: "relative",
       overflow: 'hidden',
-      margin: "100px auto",
+      margin: "350px auto",
       width: `${size}px`,
       height: `${size}px`,
-      backgroundColor: `rgba(0, 0, 0, 0.4)`,
+      backgroundColor: colors.moon,
       borderRadius: "50%",
       zIndex: 1,
     }}>
@@ -47,7 +52,7 @@ const MoonShadow = (properties:MoonProps) => {
 
   const rLeft = 50
   const rTop = (size + 4) / 2
-
+  
   return (
     <div style={{
       position: 'absolute',
@@ -55,12 +60,88 @@ const MoonShadow = (properties:MoonProps) => {
       height: `${size + 4}px`,
       width: `${size + 4}px`,
       top: '-2px',
-      left: '-2px',
-      
+      left: '-2px'
     }}>
+
+      <ShadowPartLeft illumination={illumination} size={size}/>
+      <ShadowPartRight illumination={illumination} size={size}/>
+
     </div>
   )
 }
+
+
+
+const ShadowPartLeft = (properties:MoonProps) => {
+  
+  const {illumination, size} = properties
+  const {fraction, phase} = illumination
+
+  const height = phase < 0.5 ? 
+    ((size) * (1 + fraction) * 1) + 8 :
+    (size + 8)
+  const width = height
+
+  const borderRadius = phase < 0.5 ? 
+    50 * (1 - fraction) + (fraction * 30) :
+    0
+
+  const translateX = phase < 0.5 ? 
+    (width - size) / 2 :
+    ((size / 2) + 2)
+  const translateY = (height - size) / 2
+
+  return (
+    <div style={{
+      position: 'absolute',
+      zIndex: 3,
+      height: `${height}px`,
+      width: `${height}px`,
+      top: '-2px',
+      borderRadius: `${borderRadius}%`,
+      left: `-2px` ,
+      transform: `translate(${translateX}px, -${translateY}px)`, 
+      backgroundColor: colors.moonShadow
+    }}/>
+  )
+}
+
+const ShadowPartRight = (properties:MoonProps) => {
+  
+  const {illumination, size} = properties
+  const {fraction, phase} = illumination
+  
+  const pl = 0.45
+
+  const height = phase < pl ? 
+    size + 8 :
+    ((size) * (1 + fraction) * 1) + 8
+  const width = height
+
+  const borderRadius = phase < pl ? 
+    0 :
+    50 * (1 - fraction) + (fraction * 30)
+
+  const translateX = phase < pl ? 
+    -(width / 2) -4 :
+    -(width) + (size / 2) + ((1 - fraction) * (size / 2))
+  const translateY = (height - size) / 2
+
+  return (
+    <div style={{
+      position: 'absolute',
+      zIndex: phase < pl ? 2 : 3,
+      height: `${height}px`,
+      width: `${height}px`,
+      top: '-2px',
+      borderRadius: `${borderRadius}%`,
+      left: `-2px` ,
+      transform: `translate(${translateX}px, -${translateY}px)`, 
+      backgroundColor: colors.moon
+    }}/>
+  )
+}
+
 
 
 interface CraterProps extends MoonProps {
@@ -90,7 +171,7 @@ const Crater = (properties:CraterProps) => {
       height: `${size}px`,
       borderRadius: "50%",
       backgroundColor: "#BABCC1",
-      zIndex: 3,
+      zIndex: 5,
     }}>
       <div style={{
         width: `${adjustedSize + 4}px`,
